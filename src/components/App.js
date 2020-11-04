81b8ba112110e2bfd5dfc4f5c591a99096fdf6de
Thomas Pappas <thpapei1@gmail.com>
2020-11-04 11:39:14 +0200

    Removed dummy log

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Balls from './Balls';
 import Controls from './Controls';
 import Information from './Information';
 import Players from './Players';
 import { useSelector } from 'react-redux';
+import Modal from './Modal';
 
 
 const App = (props) => {
+  const [isModalOpen, setIsModalOpen] = useState(true);
 
   const gameStarted = useSelector(state => state.game.gameStarted);
-  console.log(gameStarted);
 
   return (
     <div className='app'>
@@ -17,6 +18,7 @@ const App = (props) => {
       <Information currentFrame={3} frames={5} reds={6} difference={5} />
       <Balls />
       <Controls />
+      {isModalOpen && <Modal setIsModalOpen={setIsModalOpen}><p>Start new game?</p></Modal>}
     </div >)
 }
 
