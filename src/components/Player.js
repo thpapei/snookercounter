8d10236c007361864a787935ae77e0575328eeb3
Thomas Pappas <thpapei1@gmail.com>
2020-11-04 09:51:43 +0200

    Can change name

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,13 +1,15 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { setActivePlayerId } from '../state/slices/game';
 
 const Player = ({ name, score, gamesWon, activePlayerId, id }) => {
-
   const dispatch = useDispatch();
-  const handleNameChange = e => {
-    //dispatch name change
 
+  const [username, setUsername] = useState(name);
+
+  const handleNameChange = e => {
+    console.log(e.target.value);
+    setUsername(e.target.value);
   }
 
   const handleActivePlayer = () => {
@@ -22,7 +24,7 @@ const Player = ({ name, score, gamesWon, activePlayerId, id }) => {
   return (
     <div className='player_container'>
       <div className='player_score' style={activePlayerId === id ? style : null} onClick={handleActivePlayer} ><p>{score}</p></div>
-      <input value={name} onChange={handleNameChange} />
+      <input value={username} onChange={handleNameChange} />
       <div className='player_frames_won'>Frames Won: {gamesWon}</div>
     </div>);
 };
