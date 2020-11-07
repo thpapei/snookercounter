786c591dee2e9ea97a324c8cc1e29ab825cc05c3
Thomas Pappas <thpapei1@gmail.com>
2020-11-07 14:51:52 +0200

    Set a placeholder to player names

--- src/components/Player.js
+++ src/components/Player.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { setActivePlayerId } from '../state/slices/game';
 
-const Player = ({ name, score, gamesWon, activePlayerId, id }) => {
+const Player = ({ score, gamesWon, activePlayerId, id }) => {
   const dispatch = useDispatch();
 
   const [username, setUsername] = useState(name);
@@ -23,7 +23,7 @@ const Player = ({ name, score, gamesWon, activePlayerId, id }) => {
   return (
     <div className='player_container'>
       <div className='player_score' style={activePlayerId === id ? style : null} onClick={handleActivePlayer} ><p>{score}</p></div>
-      <input value={username} onChange={handleNameChange} />
+      <input value={username} onChange={handleNameChange} placeholder={`Player ${id}`} />
       <div className='player_frames_won'>Frames Won: {gamesWon}</div>
     </div>);
 };
