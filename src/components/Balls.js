5dee3fd9eb4a43f77d7947689739500e2e1ebe88
Thomas Pappas <thpapei1@gmail.com>
2020-11-07 12:31:05 +0200

    Reworked Balls component to show all balls

--- src/components/Balls.js
+++ src/components/Balls.js
@@ -7,38 +7,25 @@ import blue from '../assets/images/blue.png';
 import brown from '../assets/images/brown.png';
 import green from '../assets/images/green.png';
 import yellow from '../assets/images/yellow.png';
-import ballSwitch from '../assets/images/ballSwitch.png';
 
 const Balls = (props) => {
-
-  const [redVisible, setRedVisible] = useState(true);
-
   const pocketBall = points => {
     // dispatch points
     setRedVisible(!redVisible);
   }
 
-  const switchBalls = () => {
-    setRedVisible(!redVisible);
-  }
   return (
     <>
       <div className='controls balls'>
-        {redVisible && <div className='controls red'>
-          <div className='control ball red'><img src={red} alt='Restart Frame' onClick={() => pocketBall(1)} /></div>
-        </div>}
-
-        {!redVisible && <div className='controls colors'>
-          <div className='control ball yellow'><img src={yellow} alt='Restart Frame' onClick={() => pocketBall(2)} /></div>
-          <div className='control ball green'><img src={green} alt='Restart Frame' onClick={() => pocketBall(3)} /></div>
-          <div className='control ball brown'><img src={brown} alt='Restart Frame' onClick={() => pocketBall(4)} /></div>
-          <div className='control ball blue'><img src={blue} alt='Restart Frame' onClick={() => pocketBall(5)} /></div>
-          <div className='control ball pink'><img src={pink} alt='Restart Frame' onClick={() => pocketBall(6)} /></div>
-          <div className='control ball black'><img src={black} alt='Restart Frame' onClick={() => pocketBall(7)} /></div>
-          <div className='control switch'><img src={ballSwitch} onClick={() => switchBalls()} /></div>
-        </div>}
+        <div className='control ball red'><img src={red} alt='Red ball' onClick={() => pocketBall(1)} /></div>
+        <div className='control ball yellow'><img src={yellow} alt='Yellow ball' onClick={() => pocketBall(2)} /></div>
+        <div className='control ball green'><img src={green} alt='Green ball' onClick={() => pocketBall(3)} /></div>
+        <div className='control ball brown'><img src={brown} alt='Brown ball' onClick={() => pocketBall(4)} /></div>
+        <div className='control ball blue'><img src={blue} alt='Blue ball' onClick={() => pocketBall(5)} /></div>
+        <div className='control ball pink'><img src={pink} alt='Pink ball' onClick={() => pocketBall(6)} /></div>
+        <div className='control ball black'><img src={black} alt='Black ball' onClick={() => pocketBall(7)} /></div>
+        <div className='control ball white'><img src={white} alt='White ball' onClick={() => pocketBall(7)} /></div>
       </div>
-
     </>
   )
 }
