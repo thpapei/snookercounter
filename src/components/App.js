af6ecbecff83159c00537beafad872265b280a96
Thomas Pappas <thpapei1@gmail.com>
2020-11-07 21:45:16 +0200

    Added the Foul Panel to the main App component

--- src/components/App.js
+++ src/components/App.js
@@ -7,10 +7,12 @@ import { useSelector } from 'react-redux';
 import Modal from './Modal';
 import NewGameForm from './NewGameForm';
 import startIcon from '../assets/images/start.png';
+import FoulPanel from './FoulPanel';
 
 
 const App = (props) => {
   const [isStartGameModalOpen, setIsStartGameModalOpen] = useState(false);
+  const [isFoulPanelModalOpen, setIsFoulPanelModalOpen] = useState(false);
 
   const gameStarted = useSelector(state => state.game.present.gameStarted);
 
@@ -21,18 +23,29 @@ const App = (props) => {
       {gameStarted ?
         (
           <>
-            <Balls />
-            <Controls setIsModalOpen={setIsStartGameModalOpen} />
+            <Balls setIsModalOpen={setIsFoulPanelModalOpen} />
+            <Controls />
           </>
         )
         :
         (<div className='control call-to-action' onClick={() => setIsStartGameModalOpen(true)}><img src={startIcon} /></div>)}
-      {isStartGameModalOpen ?
-        <Modal setIsModalOpen={setIsStartGameModalOpen}>
-          <NewGameForm setIsModalOpen={setIsStartGameModalOpen} />
-        </Modal>
-        :
-        null}
+      {
+        isStartGameModalOpen ?
+          <Modal setIsModalOpen={setIsStartGameModalOpen}>
+            <NewGameForm setIsModalOpen={setIsStartGameModalOpen} />
+          </Modal>
+          :
+          null
+      }
+      {
+        isFoulPanelModalOpen ?
+          <Modal setIsModalOpen={setIsFoulPanelModalOpen}>
+            <FoulPanel setIsModalOpen={setIsFoulPanelModalOpen} />
+          </Modal>
+          :
+          null
+      }
+
     </div >)
 }
 
