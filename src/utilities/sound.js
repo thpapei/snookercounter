d0dc2bf0647c51b5cdf0405e0af34f0e748f409c
Thomas Pappas <thpapei1@gmail.com>
2020-11-14 15:01:11 +0200

    Added silent sound when sounds are disabled

--- src/utilities/sound.js
+++ src/utilities/sound.js
@@ -1,18 +1,23 @@
+import options from '../options.json';
+const soundsEnabled = options['sounds-enabled']
+
+const noSoundUrl = require('../assets/sounds/nosound.mp3');
+
 const useConfirmAudio = () => {
   const audioUrl = require('../assets/sounds/confirm.mp3');
-  const audio = new Audio(audioUrl);
+  const audio = new Audio(soundsEnabled ? audioUrl : noSoundUrl);
   return audio;
 }
 
 const useCancelAudio = () => {
   const audioUrl = require('../assets/sounds/cancel.mp3');
-  const audio = new Audio(audioUrl);
+  const audio = new Audio(soundsEnabled ? audioUrl : noSoundUrl);
   return audio;
 }
 
 const useStartGameAudio = () => {
   const audioUrl = require('../assets/sounds/startgame.mp3');
-  const audio = new Audio(audioUrl);
+  const audio = new Audio(soundsEnabled ? audioUrl : noSoundUrl);
   return audio;
 }
 
